9f9201a05c628babb9b24700e6de3b6986176549
dhdydtn91 <dhdydtn91@naver.com>
2019-08-21 14:56:25 +0900

    dsadsadasdsa

--- gretting.js
+++ gretting.js
@@ -1,9 +1,25 @@
 const form = document.querySelector(".js-form"),
-    input = from.querySelector("input"),
+    input = form.querySelector("input"),
     greeting = document.querySelector(".js-greetings");
 
 const USER_LS = "currentUser",
     SHOWING_CN= "showing";
+
+function saveName(text) {
+    localStorage.setItem(USER_LS,text);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit)
+}
 
 function paintGreeting(text){
     form.classList.remove(SHOWING_CN);
@@ -14,13 +30,13 @@ function paintGreeting(text){
 function loadName() {
     const currentUser = localStorage.getItem(USER_LS);
     if(currentUser ===null) {
-        // she is not
+        askForName();
     } else {
         paintGreeting(currentUser);
     }
 }
 
 function init() {
-
+    loadName();
 }
 init();
